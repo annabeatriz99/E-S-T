b78442baea13c15473c8371a7a47c288542b85c2
Anna Beatriz <astronautaviajante@hotmail.com>
2025-08-01 11:38:50 -0400

    commit

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -2,9 +2,9 @@ const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-  const nftAddress = "COLE_AQUI_O_ENDEREÇO_DO_CONTRATO";
-  const recipient = "COLE_AQUI_O_ENDEREÇO_DO_DESTINATÁRIO";
-  const tokenURI = "ipfs://CID_DO_JSON_DO_ASSET";
+  const nftAddress = "https://sepolia.etherscan.io/address/0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43";
+  const recipient = "0x8f7eA5DBa70b96f33E5d46B0a95034532a93d6a8";
+  const tokenURI = "bafkreibqpfce2mdnr5itqee4vdkzlm5r47taqs2nc6mghyavldt4ikbrhe";
 
   const RealAssetNFT = await ethers.getContractFactory("RealAssetNFT");
   const contract = await RealAssetNFT.attach(nftAddress);
